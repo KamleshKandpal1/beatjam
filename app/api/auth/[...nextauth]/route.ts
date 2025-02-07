77a3fd79bf71fb24701d81bb36b45a7602d15c0a
Kamlesh Kandpal <kkandpal007@gmail.com>
2025-02-07 15:19:38 +0530

    auth changes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  secret: process.env.AUTH_SECRET,
   callbacks: {
     async signIn(params) {
       if (!params.user.email) {
